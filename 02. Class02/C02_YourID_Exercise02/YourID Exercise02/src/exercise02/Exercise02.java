77cdf5d124d974e938187940a93538a1ab3b254a
lalobarri <eduardobarri@gmail.com>
2016-06-26 13:11:04 -0500

    El primero proyectocto de eCarnegie

---
+++ 02. Class02/C02_YourID_Exercise02/YourID Exercise02/src/exercise02/Exercise02.java
@@ -0,0 +1,10 @@
+package exercise02;
+
+public class Exercise02 {
+	
+	public static void main(String args[]){
+		System.out.println("Hello World!");
+		System.out.println("   - written by: Lynn Robert Carter");
+	}
+
+}
